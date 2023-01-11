3ece7f8406e15a9208d660794347a65e239007da
Joshua Taylor <toasterrage@gmail.com>
2023-01-11 00:17:04 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Server file...
+// Load modules
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Database connection
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to the ${process.env.DB_NAME} database.`)
+)
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
