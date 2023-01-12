f69738a4dc7dd1114e7f847e39416f269c825beb
Joshua Taylor <toasterrage@gmail.com>
2023-01-11 23:01:44 -0700

    Fixed bug

--- db/schema.sql
+++ db/schema.sql
@@ -17,9 +17,9 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30)  NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
