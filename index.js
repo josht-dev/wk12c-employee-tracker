4a8fcbd433d750ef60770098c7684d3b8b29f225
Joshua Taylor <toasterrage@gmail.com>
2023-01-11 22:51:09 -0700

    Added menu prompt

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
-// Load modules
+// *****Load modules*****
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// *****Global variables*****
 const PORT = process.env.PORT || 3001;
+// Menu prompt for inquirer package
+const menuPrompt = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menuSelect',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Quit'
+        ],
+        filter: function(menuSelect) {
+            switch (menuSelect) {
+                case 'View All Employees':
+                    return 'viewEmployees';
+                    break;
+                case 'Add Employee':
+                    return 'addEmployee';
+                    break;
+                case 'Update Employee Role':
+                    return 'updateRole';
+                    break;
+                case 'View All Roles':
+                    return 'viewRoles';
+                    break;
+                case 'Add Role':
+                    return 'addRole';
+                    break;
+                case 'View All Departments':
+                    return 'viewDepartments';
+                    break;
+                case 'Add Department':
+                    return 'addDepartment';
+                    break;
+                default:
+                    return 'quit';
+                    break;
+            }
+        }
+    }
+];
 
 // Database connection
 const db = mysql.createConnection(
@@ -16,3 +63,11 @@ const db = mysql.createConnection(
     console.log(`Connected to the ${process.env.DB_NAME} database.`)
 )
 
+// *****Run code at load*****
+
+async function menu () {
+    const answer = await inquirer.prompt(menuPrompt);
+
+}
+
+menu();
