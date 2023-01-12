225113c26af34b22c6af996bd451f6ea349ab0ac
Joshua Taylor <toasterrage@gmail.com>
2023-01-11 21:07:59 -0700

    Removed express package

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 // Load modules
-const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Database connection
 const db = mysql.createConnection(
@@ -22,7 +16,3 @@ const db = mysql.createConnection(
     console.log(`Connected to the ${process.env.DB_NAME} database.`)
 )
 
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
