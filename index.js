074435caff8a1b2c133095e1f45f0a1d3c217d7f
Joshua Taylor <toasterrage@gmail.com>
2023-01-12 09:46:34 -0700

    Working on inquirer

--- index.js
+++ index.js
@@ -51,6 +51,83 @@ const menuPrompt = [
         }
     }
 ];
+const addPrompt = [
+    {
+        type: 'input',
+        message: 'What is the name of the new department?',
+        name: 'departmentName',
+        when: function(answer) {
+            return (answer.menuSelect === 'addDepartment') ? true : false;
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is the title of the new role?',
+        name: 'roleTitle',
+        when: function(answer) {
+            return (answer.menuSelect === 'addRole') ? true : false;
+        }
+    },
+    {
+        type: 'number',
+        message: 'What is the salary of the new role?',
+        name: 'roleSalary',
+        when: function(answer) {
+            return (answer.menuSelect === 'addRole') ? true : false;
+        }
+    },
+    {
+        // TODO - change this to a type: list, choice can take a function
+        type: 'input',
+        message: 'Which department uses the new role?',
+        name: 'roleDepartment',
+        when: function(answer) {
+            return (answer.menuSelect === 'addRole') ? true : false;
+        }
+    },
+    {
+        type: 'input',
+        message: "Employee's first name?",
+        name: 'employeeFirstName',
+        when: function(answer) {
+            return (answer.menuSelect === 'addEmployee') ? true : false;
+        }
+    },
+    {
+        type: 'input',
+        message: "Employee's last name?",
+        name: 'employeeLastName',
+        when: function(answer) {
+            return (answer.menuSelect === 'addEmployee') ? true : false;
+        }
+    },
+    {
+        // TODO - change this to a type: list, choice can take a function
+        type: 'input',
+        message: "Employee's title:",
+        name: 'employeeTitle',
+        when: function(answer) {
+            return (answer.menuSelect === 'addEmployee') ? true : false;
+        }
+    },
+    {
+        type: 'confirm',
+        message: 'Does the employee have a manager?',
+        name: 'employeeManaged',
+        when: function(answer) {
+            return (answer.menuSelect === 'addEmployee') ? true : false;
+        }
+    },
+    {
+        // TODO - change this to a type: list, choice can take a function
+        type: 'input',
+        message: "Select the employee's manager:",
+        name: 'employeeManager',
+        when: function(answer) {
+            return (answer.menuSelect === 'employeeManaged') ? true : false;
+        }
+    }
+]
 
 // *****Run code at load*****
 
@@ -58,7 +135,7 @@ async function menu () {
     const answer = await inquirer.prompt(menuPrompt);
 
     //viewTable('department');
-    viewByManager(7);
+    //viewByManager(7);
 };
 
 menu();
